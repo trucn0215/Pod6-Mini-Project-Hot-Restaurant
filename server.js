055f5e82aca5319cc57bb3829819e9dc63bd0b9f
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2020-12-14 21:53:05 -0800

    updated reservation.html, applied jQuery to store data in reservations array

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const PORT = 3000
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const reservations = [];
 
@@ -17,6 +21,16 @@ app.get("/viewtable", function(req, res) {
     res.sendFile(path.join(__dirname, "viewtable.html"));
 })
 
+
+app.post("/api/reservations", function(req, res) {
+    var newReservation = req.body;
+
+    console.log(newReservation);
+  
+    reservations.push(newReservation);
+  
+    res.json(newReservation);
+  });
 
 app.listen(PORT, function() {
     console.log("App listening to PORT " + PORT)
